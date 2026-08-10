b3d8a8cd57fa0aa6151474d1dff0e0f80d1706cf
Guilherme Brito Freire <guilhermebritofreire@gmail.com>
2026-08-10 16:24:59 -0300

    refactor: removed unsused function

--- metafr_test/src/main.rs
+++ metafr_test/src/main.rs
@@ -1,11 +1,6 @@
 use metafr::{Page, document::{Document, create_document}, get, start };
 use metafr::Html;
 
-// Retorne Html(...) em vez de string pura
-pub async fn meu_handler() -> Html<&'static str> {
-    Html("<!DOCTYPE html><html><body><h1>Hello World!</h1></body></html>")
-}
-
 fn main() {
 
     let paginas = vec![
